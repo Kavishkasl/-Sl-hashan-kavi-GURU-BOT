5750e567b71a5e90ec17e1ac78e88a2ea7568a76
Kavishkasl <133832541+Kavishkasl@users.noreply.github.com>
2023-06-02 20:57:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['94773076130', 'sandaru', true],
+  ['94773076130', 'hashan kavi', true], 
   [''] 
 ] //Number of owners
 
